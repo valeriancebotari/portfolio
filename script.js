37baeb9b664a3fe114962b47418b724786083fd9
vali cebo <cebotari.valerian@gmail.com>
2023-08-01 23:07:08 +0300

    adding js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-/* ****************** JS CODE ******************** */
-console.log('sunt in js code');
+const Menu = '.menu';
+const divMenu = document.querySelector(Menu);
+const menuTrigger = document.querySelector('.menu-trigger');
+
+menuTrigger.addEventListener('click', () => {
+    setTimeout( () => {
+        if (!divMenu.classList.contains('show')) {
+            divMenu.classList.add('show');
+            document.body.classList.add('menu-visible');
+        }
+    }, 200);
+});
+
+// close menu by clicking outside
+document.addEventListener('click', (e) => {
+    const isClosest = e.target.closest(Menu);
+
+    if(!isClosest && divMenu.classList.contains('show')){
+        divMenu.classList.remove('show');
+        document.body.classList.remove('menu-visible');
+    }
+})
